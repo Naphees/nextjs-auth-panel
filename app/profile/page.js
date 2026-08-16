7f7276a6a90ede0cb9c254264238a91cd99a97b4
naphees <naphees9997@gmail.com>
2026-08-16 18:07:56 +0530

    Today, I worked on password update  form. I designed  Password reset form

--- app/profile/page.js
+++ app/profile/page.js
@@ -11,8 +11,16 @@ export default function Profile(){
             oldPassword:"",
             newPassword:""
     });
+
+    // default messate
+    const defaultMessage ={
+        oldPasswordMessage:"",
+        newPasswordMessage:"",
+        error:"",
+        success:""
+    };
     // Message state
-    const [message, setMessage] = useState("");
+    const [message, setMessage] = useState(defaultMessage);
 
     // PasswordForm Toggle 
     const [passwordFormToggle, setPasswordFormToggle] = useState(false);
@@ -30,7 +38,23 @@ export default function Profile(){
     // Password Form Submit
     async function userPasswordSubmit() {
         try{
-
+            // Default message set
+            setMessage(defaultMessage);
+                // Old Password Check
+                if(!passwordForm.oldPassword){
+                    setMessage((prev)=>({
+                        ...prev,
+                        oldPasswordMessage:" * Old password must"
+                    }));
+                    return;
+                }
+                // New Password Check
+                if(!passwordForm.newPassword) {
+                    setMessage((prev)=>({
+                        ...prev, newPasswordMessage:`* New password must`
+                    }))
+                    return;
+                }
         }catch(error){
             console.log(error.message);
         }
@@ -38,7 +62,7 @@ export default function Profile(){
     
     
     return(
-       <div>
+       <div className="">
          <div>
             <h1>Profile</h1>
             <p>Name : {data?.user?.name}</p>
@@ -47,9 +71,37 @@ export default function Profile(){
         </div>
         <div>
                     {/*User Form Details */}  
-                    <button className="p-2 m-2 bg-red-300">
+                  { passwordFormToggle  === false && ( <button className="p-2 m-2 bg-red-300" onClick={()=> setPasswordFormToggle(true)}>
                         Change Password
                     </button>
+                   ) }
+
+                    {
+                        passwordFormToggle && <div className="flex justify-center items-start ">
+                                        <div className="flex flex-col relative w-70 h-80 border border-gray-300 top-5" >
+                                                <div>
+                                                        <button className="text-xl absolute top-2 right-2" onClick={()=> setPasswordFormToggle(false)}>×</button>
+                                                             <p className="text-xl  text-left m-4"> Change Password</p>
+                                                
+                                                 </div>
+                                                 <div>
+                                                            <input type="password" name="oldPassword" value={passwordForm.oldPassword ||""} onChange={(e)=> setPasswordForm((prev)=>({ ...prev, [e.target.name]:e.target.value}))} placeholder="Enter Old Password" className="text-sm p-1 m-4 border rounded border-gray-500 "  />
+                                                            { message.oldPasswordMessage &&<p className="text-sm text-red-300"> {message.oldPasswordMessage}</p>}
+                                                </div>
+                                                <div>
+                                                            <input type="password" name="newPassword" value={passwordForm.newPassword || ""} onChange={(e)=> setPasswordForm((prev)=>({...prev,[e.target.name]:e.target.value}))} placeholder="Enter New Password" className="text-sm p-1 m-4 border rounded border-gray-500 " />
+                                                             {message.newPasswordMessage && <p className="text-sm text-red-300">{message.newPasswordMessage}</p>}
+                                                </div>
+                                                <div> 
+                                                                <button onClick={userPasswordSubmit} className="m-4 text-xl bg-green-300 p-2 rounded">
+                                                                    Update
+                                                                </button>
+                                                 </div>
+                                                
+                                    
+                            </div>
+                            </div>
+                    }
 
         </div>
        </div>
